0436c66abbe2dcddc546fb41e44cc4b501e93ea4
t.shibuta <make.app.ts@gmail.com>
2017-05-14 23:59:51 +0900

    ヘッダーとフッターを追加

---
+++ components/main-footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <span class="copyright">&copy; 2016-{{copyrightEnd}} mya-ake</span>
+  </footer>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        copyrightEnd: new Date().getFullYear(),
+      }
+    },
+  }
+</script>
+
+<style lang="postcss?sourceMap">
+.footer {
+  width: 100vw;
+  height: 30px;
+  margin-top: auto;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.copyright {
+  display: inline-block;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app">
+    <main-header></main-header>
+    <main>
+      <nuxt/>
+    </main>
+    <main-footer></main-footer>
   </div>
 </template>
+
+<script>
+  import mainHeader from '~components/main-header.vue'
+  import mainFooter from '~components/main-footer.vue'
+
+  export default {
+    components: {
+      mainHeader,
+      mainFooter,
+    },
+  }
+</script>
 
 <style lang="postcss?sourceMap">
 @import './../node_modules/normalize.css/normalize.css';
@@ -28,7 +44,11 @@ body {
   letter-spacing: 0.04em;
   -webkit-font-smoothing: antialiased;
   -moz-font-smoothing: antialiased;
-},
+}
+
+main {
+  margin-top: var(--h-main-header);
+}
 
 a {
   color: var(--c-a);
@@ -44,6 +64,12 @@ select
 }
 button {
   user-select: none;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
 @media screen and (max-width: 480px) {
