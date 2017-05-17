52277954c66d64c75f44ff6bb387a0668be797cd
t.shibuta <make.app.ts@gmail.com>
2017-05-18 00:19:25 +0900

    styleでネストが使えるように

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import')({}),
+    require('postcss-nested')({}),
     require('postcss-cssnext')({}),
     require('autoprefixer')({
       browsers: ['IE 9', 'IE 10', 'IE 11', 'last 2 versions']
