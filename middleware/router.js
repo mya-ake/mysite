a0b711ab703239f7b85a9e282d4378a361a925b1
t.shibuta <make.app.ts@gmail.com>
2017-05-20 02:17:28 +0900

    ルーターでPromise.allも使えるか確認

--- middleware/router.js
+++ middleware/router.js
@@ -1,11 +1,22 @@
-const DELAY = 2000
+const DELAY1 = 2000
+const DELAY2 = 5000
 
 const router = (context) => {
-  return new Promise((resolve, reject) => {
+  const p1 = new Promise((resolve) => {
     setTimeout(() => {
+      console.info('comp 1')
       resolve()
-    }, DELAY)
+    }, DELAY1)
   })
+
+  const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+      console.info('comp 2')
+      resolve()
+    }, DELAY2)
+  })
+
+  return Promise.all([p1, p2])
 }
 
 export default router
