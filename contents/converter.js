2e650012fa78bc657d15a20a2a8660d60fed35b3
t.shibuta <make.app.ts@gmail.com>
2017-05-22 02:48:33 +0900

    contents: コマンドの引数の取得と検証の作成

--- contents/converter.js
+++ contents/converter.js
@@ -4,5 +4,48 @@ const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 const highlight = require('highlight.js');
+const colors = require('colors/safe');
 
+/** constants */
+const ARGV = Object.freeze({
+  TARGET: {
+    NAME: 'target',
+    COMMAND: '-t',
+    CORRECT_VALUES: new Set(['pages', 'slides']),
+  },
+  FILE: {
+    NAME: 'file',
+    COMMAND: '-f',
+  },
+});
 
+/** functions */
+const extractArgv = (argv, command) => {
+  let isExtract = false;
+  return argv.filter((value) => {
+    if (/^-/.test(value) === true) {
+      isExtract = value === command;
+      return false;
+    }
+    return isExtract;
+  });
+};
+
+const verifyValues = (values, argObject) => {
+  for (let value of values) {
+    if (argObject.CORRECT_VALUES.has(value) === false) {
+      console.error(colors.red(`Error: Invalid value '${value}' in ${argObject.NAME}[${argObject.COMMAND}].`));
+      process.exit();
+    }
+  }
+};
+
+/** main process */
+const argv = process.argv.filter((value, index) => index > 1);
+
+const targets = extractArgv(argv, ARGV.TARGET.COMMAND);
+verifyValues(targets, ARGV.TARGET);
+
+const files = extractArgv(argv, ARGV.FILE.COMMAND);
+
+console.info(colors.green('Completed.'));
