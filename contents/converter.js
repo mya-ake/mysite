3f0099e7db90d270a62886460500ec7ea625dde1
t.shibuta <make.app.ts@gmail.com>
2017-05-23 00:13:38 +0900

    変換対象のファイル名を取得するところまで

--- contents/converter.js
+++ contents/converter.js
@@ -11,7 +11,7 @@ const ARGV = Object.freeze({
   TARGET: {
     NAME: 'target',
     COMMAND: '-t',
-    CORRECT_VALUES: new Set(['pages', 'slides']),
+    CORRECT_VALUES: ['pages', 'slides'],
   },
   FILE: {
     NAME: 'file',
@@ -33,19 +33,53 @@ const extractArgv = (argv, command) => {
 
 const verifyValues = (values, argObject) => {
   for (let value of values) {
-    if (argObject.CORRECT_VALUES.has(value) === false) {
+    if (argObject.CORRECT_VALUES.includes(value) === false) {
       console.error(colors.red(`Error: Invalid value '${value}' in ${argObject.NAME}[${argObject.COMMAND}].`));
       process.exit();
     }
   }
 };
 
+const createTargets = (argTargets) => {
+  let targets = [];
+  if (argTargets.length === 0) {
+    targets = Array.from(ARGV.TARGET.CORRECT_VALUES);
+  } else {
+    targets = Array.from(argTargets);
+  }
+  return targets;
+};
+
+const getFolderFileNames = (folderName) => {
+  try {
+    return fs.readdirSync(folderName);
+  } catch (err) {
+    console.error(colors.red(err));
+    process.exit();
+  }
+};
+
+const getTargetFileNames = (targets) => {
+  const fileNames = {};
+  for (let target of targets) {
+    fileNames[target] = getFolderFileNames(target);
+  }
+  return fileNames;
+};
+
 /** main process */
 const argv = process.argv.filter((value, index) => index > 1);
 
+// Targets
 const argTargets = extractArgv(argv, ARGV.TARGET.COMMAND);
 verifyValues(argTargets, ARGV.TARGET);
+const targets = createTargets(argTargets);
+console.info(colors.cyan(`Target Folders: ${targets.join(',')}`));
 
-const argFiles = extractArgv(argv, ARGV.FILE.COMMAND);
+// File Names
+const fileNames = getTargetFileNames(targets);
+console.info(fileNames);
+
+// const argFiles = extractArgv(argv, ARGV.FILE.COMMAND);
 
 console.info(colors.green('Completed.'));
