af7d3683742d5dede795ea020ffd66670d97766c
t.shibuta <make.app.ts@gmail.com>
2017-11-15 00:51:34 +0900

    scssの環境整えた

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const routes = require('./contents/routes')
 
 module.exports = {
@@ -10,16 +12,17 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
     link: [
-    ]
+    ],
   },
 
   loading: { color: '#3B8070' },
 
   css: [
     'normalize.css/normalize.css',
+    '~assets/scss/styles.scss',
   ],
 
   build: {
@@ -32,10 +35,22 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
-    }
+
+      config.module.rules
+        .find((rule) => rule.loader === 'vue-loader')
+        .options.loaders.scss
+        .push({
+          loader: 'sass-resources-loader',
+          options: {
+            resources: [
+              path.join(__dirname, 'assets', 'scss', '_variables.scss'),
+            ],
+          },
+        })
+    },
   },
 
   router: {
