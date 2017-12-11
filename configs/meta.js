4afa947facd809e3bd988ae8b994ec1571a19a6a
t.shibuta <make.app.ts@gmail.com>
2017-12-12 00:56:31 +0900

    画像をフルパスに更新

--- configs/meta.js
+++ configs/meta.js
@@ -2,8 +2,8 @@ const ORIGIN = 'https://mya-ake.com'
 const TITLE = 'mya-ake.com'
 const DESCRIPTION = '猫好きのウェブエンジニアが気まぐれで書いたり作ったりしたものを置いてるサイトです。'
 const SNS_IMAGES = {
-  SMALL: '/images/my/mya-ake-siteimage-tw.png',
-  LARGE: '/images/my/mya-ake-siteimage.png',
+  SMALL: `${ORIGIN}/images/my/mya-ake-siteimage-tw.png`,
+  LARGE: `${ORIGIN}/images/my/mya-ake-siteimage.png`,
 }
 
 const META = {
