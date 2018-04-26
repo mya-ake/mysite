4750e5bbff1535d990742277641d51760cf20d3e
mya-ake <make.app.ts@gmail.com>
2018-04-26 12:03:35 +0900

    Update

--- app/contents/routes.js
+++ app/contents/routes.js
@@ -2,6 +2,7 @@ module.exports = [
   '/posts/nuxtjs-on-aws-lambda',
   '/posts/release-info-aws-icon-searcher',
   '/posts/techbook-fest-4-neko-note',
+  '/posts/techbook-fest-4-neko-note-after',
   // '/about',
   // '/works',
   // '/slides',
